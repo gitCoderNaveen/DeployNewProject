ac6e247e414f963491823ce2e86a8d3f4df60dd7
JAVAVEEN <naveenbsc.mca1518@gmail.com>
2024-08-13 20:40:13 +0530

    updated for deploy

---
+++ backend/index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const datas = require('./Route/data')
+const mongoose = require('mongoose')
+const dataModel = require('./Model/data-model')
+const cors = require('cors')
+
+const app = express()
+
+app.use(cors())
+
+mongoose.connect('mongodb+srv://deployUser:Welcome123@newcluster.8zvx9.mongodb.net/ProductDirectory?retryWrites=true&w=majority&appName=NewCluster')
+
+mongoose.connection.on('connected', ()=>{
+    console.log('MongoDB successfully connectd')
+})
+
+app.use('/datas',datas)
+
+app.get('/',(req, res)=>{
+    res.json('hello')
+})
+
+app.listen(7002, ()=>{
+    console.log('sever is running on port 7002')
+})
